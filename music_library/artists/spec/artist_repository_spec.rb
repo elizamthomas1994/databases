8ea0d3d2ccd729c0a4ff81dac6da44758df307a0
Eliza Thomas <elizamthomas1994@gmail.com>
2022-11-22 12:22:56 +0000

    Modified repository specfile

--- music_library/artists/spec/artist_repository_spec.rb
+++ music_library/artists/spec/artist_repository_spec.rb
@@ -1,7 +1,6 @@
 require 'artist_repository'
 
 RSpec.describe ArtistRepository do
-
   def reset_artists_table
     seed_sql = File.read('spec/seeds_artists.sql')
     connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test' })
