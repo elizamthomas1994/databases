c8e8a8d8463c88ae08a4a25b75477e50b83361bb
Eliza Thomas <elizamthomas1994@gmail.com>
2022-11-25 18:51:33 +0000

    Removed previous files that were pairing session duplicates of other local files

--- 02_test_driving_repository_class_with_join/lib/cohort.rb
+++
@@ -1,7 +0,0 @@
-class Cohort
-  attr_accessor :id, :name, :starting_date, :students
-
-  def initialize
-    @students = []
-  end
-end
--- 02_test_driving_repository_class_with_join/lib/student.rb
+++
@@ -1,4 +0,0 @@
-class Student
-    attr_accessor :id, :name, :cohort_id
-  end
-  
